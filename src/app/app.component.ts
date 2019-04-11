be8d03c08e4f816a63e44416db6448b1334a2db3
dreyliky <dreyliky@gmail.com>
2019-04-11 22:37:41 +0300

    Code optimization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,51 +10,22 @@ import { FormGroup, FormControl } from '@angular/forms';
   ]
 })
 export class AppComponent implements OnInit {
-  constructor (
-    private toastsService: AngularBootstrapToastsService
-  ) {}
-
   public DefaultTitle: string    = 'Default Title';
   public DefaultMessage: string  = 'Default Message';
   public DefaultDuration: number = 5000;
 
 
-
   public ToastForm: FormGroup;
 
-  public ToastTitle: FormControl;
-  public ToastMessage: FormControl;
-  public ToastDuration: FormControl;
-  public ToastMoment: FormControl;
-  public ToastIconClass: FormControl;
-  public ToastTitleClass: FormControl;
-  public ToastBodyClass: FormControl;
-  public ToastProgressLineClass: FormControl;
-
-  public IsToastProgressLineEnabled: FormControl;
-  public IsToastDurationPausedWhenMouseEnter: FormControl;
-
   public ToolbarItems: FormGroup;
-
   public ActionButton: FormGroup;
-  public ActionButtonText: FormControl;
-  public ActionButtonClass: FormControl;
-  public ActionButtonVisible: FormControl;
-
   public CancelButton: FormGroup;
-  public CancelButtonText: FormControl;
-  public CancelButtonClass: FormControl;
-  public CancelButtonVisible: FormControl;
-
-
 
   public ContainerForm: FormGroup;
 
-  public ContainerPosition: FormControl;
-  public ContainerMarginLeft: FormControl;
-  public ContainerMarginRight: FormControl;
-  public ContainerMarginTop: FormControl;
-  public ContainerMarginBottom: FormControl;
+  constructor (
+    private toastsService: AngularBootstrapToastsService
+  ) {}
 
   ngOnInit() {
     this.toastsService.changeDefaultTitle(this.DefaultTitle);
@@ -79,7 +50,6 @@ export class AppComponent implements OnInit {
     this.toastsService.changeDefaultText(message);
   }
 
-
   public changeDefaultDuration (e: any) {
     const duration = +e.target.value;
 
@@ -103,51 +73,32 @@ export class AppComponent implements OnInit {
   }
 
   private createMainForm () {
-    this.ToastMessage                         = new FormControl('Some custom Message.<br>Here is available <b>HTML</b> tags!');
-    this.ToastTitle                           = new FormControl('Custom Title');
-    this.ToastDuration                        = new FormControl(5000);
-    this.ToastMoment                          = new FormControl('just now');
-    this.ToastIconClass                       = new FormControl('fas fa-heart text-danger');
-    this.ToastTitleClass                      = new FormControl('');
-    this.ToastBodyClass                       = new FormControl('');
-    this.ToastProgressLineClass               = new FormControl('text-danger');
-    this.IsToastProgressLineEnabled           = new FormControl(true);
-    this.IsToastDurationPausedWhenMouseEnter  = new FormControl(true);
-
     this.ToastForm = new FormGroup({
-      text: this.ToastMessage,
-      title: this.ToastTitle,
-      duration: this.ToastDuration,
-      moment: this.ToastMoment,
-      iconClass: this.ToastIconClass,
-      titleClass: this.ToastTitleClass,
-      bodyClass: this.ToastBodyClass,
-      progressLineClass: this.ToastProgressLineClass,
-      showProgressLine: this.IsToastProgressLineEnabled,
-      pauseDurationOnMouseEnter: this.IsToastDurationPausedWhenMouseEnter,
+      text: new FormControl('Some custom Message.<br>Here is available <b>HTML</b> tags!'),
+      title: new FormControl('Custom Title'),
+      duration: new FormControl(5000),
+      moment: new FormControl('just now'),
+      iconClass: new FormControl('fas fa-heart text-danger'),
+      titleClass: new FormControl(''),
+      bodyClass: new FormControl(''),
+      progressLineClass: new FormControl('text-danger'),
+      showProgressLine: new FormControl(true),
+      pauseDurationOnMouseEnter: new FormControl(true),
       toolbarItems: this.ToolbarItems
     });
   }
 
   private createToolbarItemsForm () {
-    this.ActionButtonText    = new FormControl('Confirm action');
-    this.ActionButtonClass   = new FormControl('btn btn-success btn-sm');
-    this.ActionButtonVisible = new FormControl(true);
-
-    this.CancelButtonText    = new FormControl('Cancel');
-    this.CancelButtonClass   = new FormControl('btn btn-secondary btn-sm');
-    this.CancelButtonVisible = new FormControl(true);
-
     this.ActionButton = new FormGroup({
-      text: this.ActionButtonText,
-      class: this.ActionButtonClass,
-      visible: this.ActionButtonVisible
+      text: new FormControl('Confirm action'),
+      class: new FormControl('btn btn-success btn-sm'),
+      visible: new FormControl(true)
     });
 
     this.CancelButton = new FormGroup({
-      text: this.CancelButtonText,
-      class: this.CancelButtonClass,
-      visible: this.CancelButtonVisible
+      text: new FormControl('Cancel'),
+      class: new FormControl('btn btn-secondary btn-sm'),
+      visible: new FormControl(true)
     });
 
     this.ToolbarItems = new FormGroup({
@@ -159,19 +110,12 @@ export class AppComponent implements OnInit {
   private createContainerForm () {
     const defaultMargin: string = '10px';
 
-    this.ContainerPosition      = new FormControl('topRight');
-
-    this.ContainerMarginLeft    = new FormControl(defaultMargin);
-    this.ContainerMarginRight   = new FormControl(defaultMargin);
-    this.ContainerMarginTop     = new FormControl(defaultMargin);
-    this.ContainerMarginBottom  = new FormControl(defaultMargin);
-
     this.ContainerForm = new FormGroup({
-      position: this.ContainerPosition,
-      marginLeft: this.ContainerMarginLeft,
-      marginRight: this.ContainerMarginRight,
-      marginTop: this.ContainerMarginTop,
-      marginBottom: this.ContainerMarginBottom
+      position: new FormControl('topRight'),
+      marginLeft: new FormControl(defaultMargin),
+      marginRight: new FormControl(defaultMargin),
+      marginTop: new FormControl(defaultMargin),
+      marginBottom: new FormControl(defaultMargin)
     });
   }
 }
